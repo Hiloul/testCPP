8c4bb7b1b3b0fb80c4def957bef09568ff7ce433
Hilel Hamidi <hilel.hamidi@gmail.com>
2024-01-05 22:33:00 +0100

    CPP TEST

---
+++ chrono.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <chrono>
+#inc lude <thread>
+
+#ifdef _WIN32
+#include <windows.h> // Pour Sleep sur Windows
+#endif
+
+int main() {
+    // Affiche "Hello, World!"
+    std::cout << "Hello, World!" "HOW ARE YOU" << std::endl;
+
+    // Ajoute un délai de 5 secondes (vous pouvez ajuster la durée selon vos besoins)
+#ifdef _WIN32
+    Sleep(5000); // Délai en millisecondes sur Windows
+#endif
+
+    // Termine le programme
+    return 0;
+}
---
+++ dialogue.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+class Characters {
+    public:
+        std::string name;
+        Characters(std::string name) : name(name)
+    {}
+        void talk(std:: string dialogue) {
+            std::cout << name << ": " << dialogue << std::endl;
+        }
+};
+
+int main(){
+    Characters characters ("Hiloul");
+    characters.talk("Coucou ! Je test le C++");
+        return 0;
+}
